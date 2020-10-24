cb783d2c13a28350164b27607d7725c2ed9a8c4c
Mai Thành Thắng <52226980+Thang1102@users.noreply.github.com>
2020-10-24 14:29:03 +0700

    Add files via upload

---
+++ Basic/test2.py
@@ -0,0 +1,13 @@
+import math
+a = math.factorial(5) #giai thua 5
+b = math.ceil(4.1) #lam tron len
+c =math.floor(4.9) #lam tron xuong
+#boolen
+ss1 = 10 > 8
+ss2 = 10 < 8
+
+print(a)
+print(b)
+print (c)
+print(ss1)
+print (ss2)
