6770b957cfb7a557c6a04b129b5c47824a1cd8a5
Mai Thành Thắng <52226980+Thang1102@users.noreply.github.com>
2020-10-24 15:05:29 +0700

    Add files via upload

---
+++ Basic/Number&boolen.py
@@ -0,0 +1,30 @@
+#number and boolen
+n1 = 900
+print (n1)
+n2 = type(n1) #Kieu cua bien
+print (n2)
+n3 = 100000000000000000000000
+print (n3)
+s = 2 ** 2 # so mu
+print(s)
+s = s+1
+print (s)
+s1 = 5 * s
+print (s)
+f = 9.5
+type(f) 
+c1 = 80
+c2 = 115.5
+c = c1 + c2
+print(type(c),id(c))
+print(c)
+a = 10
+b = 5
+tong = a + b
+print(tong)
+hieu = a - b
+print(hieu)
+thuong = a / b
+print(thuong) #So nay dang fload
+laydu = 10 % 3
+print(laydu)
